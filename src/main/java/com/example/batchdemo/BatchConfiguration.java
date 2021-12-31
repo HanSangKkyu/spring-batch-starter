9a62c1d29d74826c00896ae2bf016cec2a3f1edb
han_sangkyu <kkyu67@naver.com>
2021-12-31 16:47:35 +0900

    FlatFileItemReader/FlatFileItemWriter example

--- src/main/java/com/example/batchdemo/BatchConfiguration.java
+++ src/main/java/com/example/batchdemo/BatchConfiguration.java
@@ -10,13 +10,20 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
-import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.*;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.batch.item.database.support.ListPreparedStatementSetter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterUtils;
@@ -42,7 +49,8 @@ public class BatchConfiguration {
         return jobBuilderFactory.get("job")
                 .incrementer(new RunIdIncrementer())
                 .listener(jobListener) // 리스터 설정
-                .start(step(WILL_BE_INJECTED))
+                .start(flatstep()) // 파일에서 읽어서 파일에 쓰기 step
+//                .start(step(WILL_BE_INJECTED))
 //                .next(step(WILL_BE_INJECTED)) // 이렇게 연결 가능
 //                .next(step(WILL_BE_INJECTED))
 //                .next(step(WILL_BE_INJECTED))
@@ -63,6 +71,21 @@ public class BatchConfiguration {
                 .processor(itemProcessor())// 프로세서 설정
 //                .writer(itemWriter()) // DB 접근 없다면 이렇게 가능
                 .writer(jdbcBatchItemWriter(dataSource))
+                .build();
+    }
+
+
+
+    @Bean
+    @JobScope
+    public Step flatstep(){
+        return  stepBuilderFactory
+                .get("flatstep") // 이 값은 유니크 해야 한다. DB에 저장되어서 관리되기 때문에 배치를 실행할 때마다 다른 값을 넣어주어야 한다.
+                .allowStartIfComplete(true) // 한번 실행된 step도 재실행 될 수 있게 허용한다. 순서도 중요하다. 초반에 배치해야 한다.
+                .listener(stepListener) // 리스터를 달아준다. 초반에 배치해야 한다.
+                .<User,User>chunk(1)// 100개의 데이터를 chunk 사이즈 10으로 해서 10번 수행할 수 있습니다.
+                .reader(flatFileItemReader(WILL_BE_INJECTED))// 리더를 설정하고 매개변수를 null로 두면 @Value("#{jobParameters[name]}") String name에 매칭되는 값이 들어갈 것임
+                .writer(flatFileItemWriter(WILL_BE_INJECTED))
                 .build();
     }
 
@@ -106,6 +129,32 @@ public class BatchConfiguration {
                 .rowMapper(new BeanPropertyRowMapper<>(User.class))
                 .sql(NamedParameterUtils.substituteNamedParameters(sql, new MapSqlParameterSource(namedParameters)))
                 .preparedStatementSetter(new ListPreparedStatementSetter(Arrays.asList(NamedParameterUtils.buildValueArray(sql, namedParameters))))
+                .build();
+    }
+
+
+    @Bean
+    @StepScope
+    public FlatFileItemReader<User> flatFileItemReader(@Value("#{jobParameters[inputPath]}") String inputPath){
+        return new FlatFileItemReaderBuilder<User>()
+                .name("flatFileItemReader")
+                .resource(new FileSystemResource(inputPath))
+                .delimited().delimiter(",")
+                .names(new String[]{"id", "name"})
+                .fieldSetMapper(new BeanWrapperFieldSetMapper<User>() {{
+                    setTargetType(User.class);
+                }})
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public FlatFileItemWriter<User> flatFileItemWriter(@Value("#{jobParameters[outputPath]}") String outputPath){
+        return new FlatFileItemWriterBuilder<User>()
+                .name("flatFileItemWriter")
+                .resource(new FileSystemResource(outputPath))
+                .delimited().delimiter(",")
+                .names(new String[] {"id", "name"})
                 .build();
     }
 
--- src/main/java/com/example/batchdemo/BatchDemoApplication.java
+++ src/main/java/com/example/batchdemo/BatchDemoApplication.java
@@ -13,7 +13,10 @@ public class BatchDemoApplication {
     public static void main(String[] args) {
         log.info("실행은 잘 되었습니다.");
 //        SpringApplication.run(BatchDemoApplication.class, args);
-        SpringApplication.run(BatchDemoApplication.class, new String[]{"-name=ssss"}); // 프로그램 arguments를 설정한다. #{jobParameters[name]}에 들어가게 될 것이다.
+//        SpringApplication.run(BatchDemoApplication.class, new String[]{"-name=ssss"}); // 프로그램 arguments를 설정한다. #{jobParameters[name]}에 들어가게 될 것이다.
+    SpringApplication.run(BatchDemoApplication.class, new String[]{"-inputPath=/Users/han/Documents/docker/input.csv","-outputPath=/Users/han/Documents/docker/output.csv"});
+
     }
+
 
 }
